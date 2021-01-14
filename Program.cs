12d019309453dadeb142df6ac8e4f57e92c3fb93
LeonhardFischl <leonhard.fischl@exxeta.com>
2021-01-14 11:42:05 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
